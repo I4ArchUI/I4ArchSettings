fa9405d0b706247954e713c4867353a5d44e32f6
I4ArchUI <tadangkhoa.2x4@gmail.com>
2026-02-02 08:40:34 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+npm run tauri build
+
+# Install I4ArchSettings
+sudo install -Dm755 \
+src-tauri/target/release/I4ArchSettings \
+/usr/bin/i4archsettings
+
+# Copy to config directory
+cp src-tauri/target/release/I4ArchSettings ~/.config/IHyprVN/apps/i4archsettings
